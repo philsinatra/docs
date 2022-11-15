feb4a2a978592db676b02f9e3e3898c316fd0758
Phil Sinatra <code@philsinatra.com>
2022-11-15 10:22:41 -0500

    Create numeric_separators.js

---
+++ javascript/numeric_separators.js
@@ -0,0 +1,12 @@
+// Format large numbers in JavaScript with numeric separators.
+// This doesn't affect how the number works or is displayed.
+const milly = 1_000_000
+console.log(milly) // 1000000
+
+const large = 4_234_444_574
+const timeout = 32_000
+const price_in_cents = 100_99
+const price = 10_234.34
+const phone = 416_555_1234
+
+// https://twitter.com/wesbos/status/1592526511004942338
